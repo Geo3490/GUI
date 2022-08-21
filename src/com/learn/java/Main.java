8a5d407c7986a50431854fb8e116773cc3d4aea8
Geo3490 <geo40127@gmail.com>
2022-08-21 09:36:58 -0400

    updated-gui

--- src/com/learn/java/Main.java
+++ src/com/learn/java/Main.java
@@ -9,8 +9,10 @@ public class Main {
             JPanel panel = new JPanel();
             JLabel userLabel = new JLabel("User");
             JLabel passwordLabel = new JLabel("Password");
+            JLabel buttonLabel = new JLabel("Log In");
             JTextField userText = new JTextField();
             JTextField passwordText = new JTextField();
+            JButton button = new JButton();
 
             frame.setSize(350,200);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,10 +22,20 @@ public class Main {
             panel.setLayout(null);
             panel.add(userLabel);
             panel.add(passwordLabel);
+            userLabel.setBounds(20,20,80,20);
+            passwordLabel.setBounds(20,60,80,20);
+
+            panel.add(userText);
+            userText.setBounds(100,20,165,25);
+            panel.add(passwordText);
+            passwordText.setBounds(100,60, 165, 25);
+
+            panel.add(buttonLabel);
+            panel.add(button);
+            buttonLabel.setBounds(105,100, 40,20);
+            button.setBounds(100,100,45,20);
 
 
-            userLabel.setBounds(10,25,80,20);
-            passwordLabel.setBounds(10,50,80,20);
 
 
 
